7f52ef9b552806409ee5d746829ddeacdba8cff1
less_J <mgz.less@gmail.com>
2026-06-16 17:31:18 +0900

    Capacitor iOS 설정: HashRouter 연동, 앱 아이콘 추가, vite base 경로 설정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+
+/* 앱(Capacitor)환경에서는 서버가 없으므로 HashRouter 사용 */
+const isApp = window.Capacitor?.isNativePlatform?.() ?? false
+const Router = isApp ? HashRouter : BrowserRouter
 import { HelmetProvider } from 'react-helmet-async'
 import { UserProvider } from './context/UserContext'
 import App from './App'
@@ -9,11 +13,11 @@ import './App.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <Router>
         <UserProvider>
           <App />
         </UserProvider>
-      </BrowserRouter>
+      </Router>
     </HelmetProvider>
   </React.StrictMode>
 )
